8e7233b0cbe5fa656106b16a255f1a0092ded4d7
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2025-06-15 21:11:48 -0400

    reportes-acceso fixed

--- server.js
+++ server.js
@@ -29,12 +29,12 @@ app.get('/api/ping', (_req, res) => res.json({ ok: true })); // Ruta para prueba
 
 // Importa tus rutas
 app.use('/api/socios', require('./routes/socios'));
-app.use('/api/empleado', require('./routes/empleados.js'));
+app.use('/api/empleado', require('./routes/empleados'));
 app.use('/api/roles', require('./routes/roles'));
 app.use('/api/entradas', require('./routes/entradas'));
 app.use('/api/permisos', require('./routes/permisos'));
 app.use('/api/eventos', require('./routes/eventos'));
-app.use('/api/reportesAcceso', require('./routes/reportesAcceso'));
+app.use('/api/reportes-acceso', require('./routes/reportesAcceso'));
 
 // Ruta por defecto (404)
 app.use((_req, res) => res.status(404).json({ error: 'Ruta no encontrada' }));
