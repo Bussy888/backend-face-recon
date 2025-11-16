583fc4f00b003b82d582cb948c10475dee877c1a
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2025-11-16 19:51:45 -0400

    ohter changes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/permisos', require('./routes/permisos'));
 app.use('/api/eventos', require('./routes/eventos'));
 app.use('/api/reportes-acceso', require('./routes/reportesAcceso'));
 app.use('/api/tipos-socio', require('./routes/tipoSocio'));
-app.use('/api/auth', require('./routes/auth'));
+//app.use('/api/auth', require('./routes/auth'));
 
 app.use((_req, res) => res.status(404).json({ error: 'Ruta no encontrada' }));
 
